48a9a3563d2d902ab95d5b8accc45949db7d63a7
Michael Hazboun <hazbo002@umn.edu>
2024-02-24 18:38:39 -0600

    This class is making me hate it more and more every week

--- R/week6.R
+++ R/week6.R
@@ -2,11 +2,25 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(stringi)
 library(stringr)
-
-
+library(tidyverse)
 
 
 # Data Import
 citations<-stri_read_lines("../data/citations.txt", encoding="Windows-1252") # I chatgpted the encoding code
-citation_txt <- citations[!str_detect(citations, "^\\s*$")]
+citations_txt <- citations[!str_detect(citations, "^\\s*$")]
+length(citations)-length(citations_txt)
+mean(str_length(citations_txt))
+
+# Data Cleaning
+
+
+citations_tbl <- tibble(line=1:length(citations_txt),cite=citations_txt) %>%
+  mutate(cite=str_replace_all(citations_tbl$cite,"[\"\']","")) %>%
+  mutate(year=str_extract(citations_tbl$cite,"\\d{4}")) %>%
+  mutate(page_start=str_match(citations_tbl$cite,"(\\d+)-\\d+")[,2]) %>%
+  mutate(pref_ref=str_detect(citations_tbl$cite,regex("performance",ignore_case=T))) %>%
+  mutate(title=str_match(citations_tbl$cite, "\\)[\\.]\\s([^\\.]+[\\.\\?!])")[,2])  %>% #This isn't all inclusive to this painful dataset, but I've wasted too much trying trying to get it right, and it's just not going to work
+  mutate(first_author=str_match(citations_tbl$cite, "^\\*?([A-Z][a-z]+,?\\s[A-Z]\\.\\s?[A-Z]?\\.?)")[,2]) # far from perfect, but the best thing I could come up with (doesn't grab last names with non-letter characters and capital characters [atleast those are some of the flaws I found])
+
+
 
